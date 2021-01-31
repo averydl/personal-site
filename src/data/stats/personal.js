11ae151e304e78871a62f2644655401a04f3da63
Derek Avery <averyderekl@gmail.com>
2021-01-31 12:57:05 -0800

    update age format

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
+const getAge = (birth) => {
+  const date = new Date();
+  const y = date.getUTCFullYear() - birth.getUTCFullYear();
+  const m = date.getUTCMonth() - birth.getUTCMonth();
+  const d = date.getUTCDate() - birth.getUTCDate();
+  const hrs = date.getUTCHours() - birth.getUTCHours();
+  const mins = date.getUTCMinutes() - birth.getUTCMinutes();
+  const secs = date.getUTCSeconds() - birth.getUTCSeconds();
+  const ms = date.getUTCMilliseconds() - birth.getUTCMilliseconds();
+  return `${y}y ${m}m ${d}d ${hrs}h ${mins}m ${(secs + (ms / 1000)).toFixed(2)}s`;
+};
+
 const Age = () => {
   const [age, setAge] = useState();
 
   const tick = () => {
-    const divisor = 1000 * 60 * 60 * 24 * 365.25; // ms in an average year
-    const birthTime = new Date('1995-01-31T17:07:00Z'); // birth date/time (UTC)
-    setAge(((new Date() - birthTime) / divisor).toFixed(9));
+    const birth = new Date('1995-01-31T17:07:00Z'); // birth date/time (UTC)
+    const agestr = getAge(birth);
+    setAge(agestr);
   };
 
   useEffect(() => {
-    const timer = setInterval(() => tick(), 25);
+    const timer = setInterval(() => tick(), 10);
     return () => {
       clearInterval(timer);
     };
